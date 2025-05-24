7437bd3b27ad35a9e2208549e800e6b3f41ed30a
Darine Abdelmotalib <darine.motalib@gmail.com>
2025-05-24 16:30:39 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './App.scss'
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import {Routes, Route, BrowserRouter } from 'react-router-dom';
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    // <HashRouter>
-    //   <Routes>
-    //     <Route path="/" element={<HomePage />}></Route>
-    //   </Routes>
-    // </HashRouter>
-
-    <p>hello</p>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
